c59fb54b9873c27cd4112bf3b12b66c62ed576f7
kentookura <50913410+kentookura@users.noreply.github.com>
2019-05-22 14:39:30 +0200

    Update congruencesolver.py

--- congruencesolver.py
+++ congruencesolver.py
@@ -11,6 +11,7 @@ def ggT(a, b):
         a, b = b, a % b
     return a
 
+# isolve solves diophantine equations of the form ax + by = c and returns [x, y]
 def isolve(a,b,c):
     q, r = divmod(a,b)
     if r == 0:
@@ -23,29 +24,31 @@ def isolve(a,b,c):
 
 def solvecongruence(a, Verbose=False):
     d = ggT(a[0], a[2])
-    
     f = isolve(a[0], a[2], d)
     
     solution = int(f[0]*a[1]/d)
     
     c = int(a[1]/d)
     
-    print("the equation %d x \u2261 %d mod %d has " %(a[0], a[1], a[2]), int((d%a[2])), " solutions.")
-    
-    
-    return solution
+    for i in range(d):
+            return int((solution + i * c) % a[2])
 
     if Verbose:
+        print("the equation %d x \u2261 %d mod %d has " %(a[0], a[1], a[2]), int((d%a[2])), " solutions.")
         for i in range(d):
             print(int(solution + i * c) % a[2])
 
 def solsimcon():
+    # read in coefficients of a system of linear congruences in arrays
     i = int(input("how many equations? "))
     M = [int(x) for x in input("enter moduli: ").split()]
     B = [int(x) for x in input("enter B's: ").split()]
     
+    # generate array of new coefficients m[i] = prod(M)/M[i]
     m = [np.prod(M)/M[j] for j in range(i)]
+    # create array of solutions of new congruences with new coefficients 
     sol = [solvecongruence([m[j], 1, M[j]]) for j in range(i)]
+    # construct final solution by multiplying indexwise, summing the array and "modding out"
     prod = [B[j]*m[j]*sol[j] for j in range(i)]
     finalsol = [sum(prod)]%np.prod(M)
     print(finalsol)
