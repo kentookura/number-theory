b6a749c8a7bb63538567c756b90052856dacf816
Kento Okura <okurak99@u.univie.ac.at>
2019-05-27 09:17:18 +0200

    minor debugging

--- congruencesolver.py
+++ congruencesolver.py
@@ -4,8 +4,8 @@ Created on Mon May 20 13:31:36 2019
 @author: okurak99
 """
 import numpy as np
-import sys
 import math as m
+import sys
 
 
 def coprime(a):
@@ -26,12 +26,10 @@ def isolve(a,b,c):
 
 def solveCongruence(a, Verbose=False):
 
-    d = m.gcd(a[0], a[2])
+    d = m.gcd(int(a[0]), int(a[2]))
     # solve equation (x'*a)+(k'*m) = d
     f = isolve(a[0], a[2], d)
-
     solution = int(f[0]*a[1]/d)
-
     c = int(a[1]/d)
 
     for i in range(int(d)):
@@ -45,7 +43,7 @@ def solveCongruence(a, Verbose=False):
 def solvegeneralSystem(i, B, M):
 
     # generate array of new coefficients m[i] = prod(M)/M[i]
-    m = [np.prod(M)/M[j] for j in range(i)]
+    m = [int(np.prod(M)/M[j]) for j in range(i)]
     # create array of solutions of new congruences with new coefficients
     sol = [solveCongruence([m[j], 1, M[j]]) for j in range(i)]
     # construct final solution by multiplying indexwise, summing the array and "modding out"
@@ -58,7 +56,8 @@ def solvespecialSystem(i, A, B, M):
     f = [isolve(A[j]/D[j], M[j]/D[j], 1) for j in range(i)]
     C = [f[j][0] for j in range(i)]
     # convert equations to general form and solve using solvegeneralSystem
-    newB, newM = [B[j]/D[j]*C[j]], [M[j]/D[j]] for j in range(i)
+    newB = [B[j]/D[j]*C[j] for j in range(i)]
+    newM = [M[j]/D[j] for j in range(i)]
     solvegeneralSystem(i, newB, newM)
 
 def solveCongruenceinput():
@@ -72,7 +71,6 @@ def solvegeneralSysteminput():
     # exit if moduli are not coprime (no solutions)
     if not coprime(M):
         print('moduli are not comprime')
-        pass
     B = [int(x) for x in input("enter B's: ").split()]
 
     solvegeneralSystem(i, B, M)
@@ -82,7 +80,6 @@ def solvespecialSysteminput():
     M = [int(x) for x in input("enter moduli: ").split()]
     if not coprime(M):
         print('moduli are not comprime')
-        pass
     A = [int(x) for x in input("enter a's: ").split()]
     B = [int(x) for x in input("enter b's: ").split()]
 
@@ -90,8 +87,8 @@ def solvespecialSysteminput():
 
 def Help():
     print("""1: solve a linear congruence of the form ax\u2261b(mod m)\n
-2: solve a system of linear congruences x\u2261bi(mod mi)\n
-3: solve a system of linear congruences ax\u2261bi(mod mi)""")
+2: solve a system of linear congruences x\u2261b\u1d62(mod m\u1d62)\n
+3: solve a system of linear congruences a\u1d62x\u2261b\u1d62(mod m\u1d62)""")
 
 function_dict = {
     '1' : solveCongruenceinput,
@@ -100,8 +97,8 @@ function_dict = {
     'h' : Help,
     'q' : sys.exit
 }
-print('What would you like to do? (enter h for more info)\nq to quit\n')
+print('What would you like to do? (enter h for more info, q to quit)')
 while True:
     option = input()
-    func = function_dict.get(option, lambda: 'invalid option')
+    func = function_dict.get(option, lambda: 'invalid option')()
     func()
